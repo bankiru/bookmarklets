76b35e2eb5fb952852f438b877a83e4eb08f8e0e
Boris Gorbylev <ekho@ekho.name>
2016-05-24 23:50:59 +0300

    Update bamboo-quarantine.js

--- bamboo-quarantine.js
+++ bamboo-quarantine.js
@@ -1,1 +1,13 @@
-alert('HI')
+if (window.location.host != 'bamboo.devbanki.ru') {
+  alert('Domain ' + window.location.host + ' unsupported');
+} else if (window.location.pathname.match(/^\/browse\/[A-Z0-9-]+\/test$/)) {
+  (function($){
+    alert('Tests page'); 
+  })(jQuery);
+} else if (window.location.pathname.match(/^\/browse\/[A-Z0-9-]+\/quarantine$/)) {
+  (function($){
+    alert('Quaratine page'); 
+  })(jQuery);
+} else {
+  alert('Location ' + window.location.pathname + ' unsupported');
+}
