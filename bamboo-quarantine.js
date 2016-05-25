9d182a3643927898707fe4fcc3625ce140754ca0
Boris Gorbylev <ekho@ekho.name>
2016-05-25 14:10:34 +0300

    Update bamboo-quarantine.js

--- bamboo-quarantine.js
+++ bamboo-quarantine.js
@@ -5,20 +5,53 @@
       testId: $(this).data("test-id")
     }
   }
-  
+
   var doRequests = function(action, tests) {
-    $.each(tests, function(i, test){
-      console.log(action, test.planKey, test.testId);
+    var def = $.Deferred();
+
+    var totalCount = tests.length
+    var processedCount = 0;
+    var resolvedCount = 0;
+    var rejectedCount = 0;
+
+    $.each(tests, function(i, test) {
       $.ajax({
         url: window.location.origin + '/rest/api/latest/plan/' + test.planKey + '/test/' + test.testId + '/' + action,
         type:"POST",
         contentType:"application/json; charset=utf-8",
         dataType:"json",
         data:"{}",
+      })
+        .always(function() {
+          console.debug('ajax.always', arguments);
+          processedCount++;
+
+          def.notify(arg, processedCount, totalCount)
+
+          if (processedCount == totalCount) {
+            def.resolve(totalCount, resolvedCount, rejectedCount);
+          }
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          console.error('ajax.fail', arguments);
+          rejectedCount++;
+        })
+        .done(function(result) {
+          console.debug('ajax.done', arguments);
+          resolvedCount++;
+        })
+    })
+
+    def
+      .progress(function() {
+        console.debug('PROGRESS', arguments)
+      })
+      .done(function(totalCount, resolvedCount, rejectedCount) {
+        console.debug('DONE', arguments);
+        alert("" + totalCount + " test(s) " + action + " finished.\n" + (rejectedCount > 0 ? 'Some api requests failed. See console log.' : ''));
       });
-    });
   }
-  
+
   if (!window.location.host.match(/^bamboo\.(dev)?banki\.ru$/)) {
     alert('Domain ' + window.location.host + ' unsupported');
   } else if (window.location.pathname.match(/^\/browse\/[A-Z0-9-]+\/test$/)) {
